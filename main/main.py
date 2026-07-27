80723be229cfa6127de80755b8c4c7972984f4b3
sxl1 <rew2540@gmail.com>
2026-07-28 05:53:31 +0900

    dic : water-price

---
+++ main/main.py
@@ -0,0 +1,23 @@
+#main program
+
+#water price 
+water_price = {
+    "1":("cola",25),
+    "2":("coco",20),
+    "3":("water",10)
+}
+
+def menu ():
+    pass
+
+
+
+#main program
+def main ():
+    pass
+
+
+
+
+if __name__ == "__main__":
+    main()
